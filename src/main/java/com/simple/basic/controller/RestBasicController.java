b762cabcffe314ee7876c9a13cc5f26a5a352b9e
sschoi1994 <sschoi1994@naver.com>
2023-02-17 17:47:20 +0900

    restController practice with Jquery

--- src/main/java/com/simple/basic/controller/RestBasicController.java
+++ src/main/java/com/simple/basic/controller/RestBasicController.java
@@ -33,8 +33,12 @@ public class RestBasicController {
 	
 	//객체를 담게 되면 application/json 형식으로 반환하게 된다.
 	//@getmapping의 produces는 생략 가능 // 내가 보내는 타입의 형식을 지정함.
-	//text/plain => String    // application/json   => Json형식
-	//consumes 받는데이터에 대한 타입
+	/*
+	 * consumes = 받는데이터에 대한 타입 (생략시 json)
+	 * produces = 보내는데이터에 대한 타입 (생략시 json) 
+	 * 
+	 * text/plain => String    // application/json   => Json형식
+	 */
 	@GetMapping(value = "/getObject", produces = "application/json")
 	public SimpleVO getObject() {
 		SimpleVO vo = new SimpleVO(1,"aaa123","홍길동");
@@ -75,6 +79,7 @@ public class RestBasicController {
 	
 	//get형식 값을 받는 방법 2 = 쿼리파람 url/키/키
 	//http://localhost:8383/getKey/
+	@CrossOrigin("http://localhost:3000")
 	@GetMapping("/getKey2/{sort}/{apiKey}")
 	public String getKey2(@PathVariable("sort") String sort,
 							@PathVariable("apiKey") String key) {
@@ -86,9 +91,9 @@ public class RestBasicController {
 	//////////////////////////post방식의 처리
 	//값을 받는 방법 1- VO로 맵핑
 	//JSON형식이 데이터를 자바의 객체로 맵핑시키려면 @RequestBody 어노테이션을 사용해야한다.
-	
+
 	//cors - 기본적으로 서버가 다르면 요청을 거절하는데 (특정 서버에 대하여 허용해줌)
-	//@CrossOrigin(*)모든곳에서 다열림 ( 보안상 좋지 않음)
+	//@CrossOrigin(*)모든곳에서 다열림 ( 보안상 좋지 않음) //혹은 필터를 통해서 설정가능
 	@CrossOrigin("http://localhost:3000")
 	@PostMapping("/getJson")
 	public String getJson(@RequestBody SimpleVO vo) {
@@ -135,4 +140,27 @@ public class RestBasicController {
 		
 	}
 	
-}
+	//jquery - ajax
+	
+	@CrossOrigin("http://127.0.0.1:5501")
+	@PostMapping("/getAjax")
+	public Map<String, Object> getAjax(@RequestBody SimpleVO simpleVo){
+		//받은 데이터
+		System.out.println(simpleVo.toString());
+		
+		//보낸 데이터
+		Map<String, Object> map = new HashMap<>();
+		SimpleVO vo = new SimpleVO(1,"aaa123","홍길동");
+		map.put("total", 100);
+		map.put("data", vo);
+		
+		return map;
+	}
+	
+	@CrossOrigin("http://127.0.0.1:5501")
+	@GetMapping("/getAjax2/{topic}")
+	public String getAjax2(@PathVariable("topic") String topic) {
+		System.out.println(topic);
+		return "success";
+	}
+s}
